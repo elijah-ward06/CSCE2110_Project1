8373b371844eaf479284286b769c680e2e5fa1d9
Ademola D. <101699902+ademolad18@users.noreply.github.com>
2026-03-29 20:56:33 -0500

    Add Driver class definition in driver.hpp

---
+++ driver/driver.hpp
@@ -0,0 +1,35 @@
+#ifndef DRIVER_HPP
+#define DRIVER_HPP
+
+#include "tools/date.hpp"
+#include "tools/location.hpp"
+#include "tickets/tickets.hpp"
+#include <string>
+using namespace std;
+
+class Driver {
+private:
+    int id;
+    string name;
+    Date dob;
+    Location location;
+
+    string workType;
+    int experienceYears;
+    string medicalCondition;
+
+    Date licenseDate;
+    Tickets tickets;
+
+public:
+    Driver(int id, string name, Date dob, Location loc,
+           string work, int exp, string med, Date licenseDate);
+
+    int getID();
+    Date getLicenseDate();
+
+    void addTicket(Ticket t);
+    void display();
+};
+
+#endif
